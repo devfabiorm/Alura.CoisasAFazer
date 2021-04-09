5cb4a4f471bf63b62137dc0efff4a5c97f1798cd
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2021-04-08 22:51:33 -0300

    Criando dublê de repositório para testar o cadastro de tarefas

---
+++ tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -0,0 +1,29 @@
+﻿using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Services.Handlers;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class CadastraTarefaHandlerExecute
+    {
+        [Fact]
+        public void DadaTarefaComInfoValidasDeveIncluirNoDb()
+        {
+            //Arrange
+            var comando = new CadastraTarefa("Estudar xUnit", new Categoria("Estudo"), new DateTime(2021, 12, 31));
+            var repo = new RepositorioFake();
+            var handler = new CadastraTarefaHandler(repo);
+
+            //Act
+            handler.Execute(comando);
+
+            //Assert
+            var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar xUnit").FirstOrDefault();
+            Assert.NotNull(tarefa);
+        }
+    }
+}
+
